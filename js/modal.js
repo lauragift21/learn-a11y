8cb6484c2ad44d3283e451297b1c52fb3e0a827c
Gift <laurandidi21@gmailcom>
2019-03-10 02:48:41 +0100

    Add exercise 2 solution

--- js/modal.js
+++ js/modal.js
@@ -1,5 +1,5 @@
-var modal        = document.querySelector('.focus-modal');
-var modalButton  = document.querySelector('.focus-modal-button');
+var modal = document.querySelector('.focus-modal');
+var modalButton = document.querySelector('.focus-modal-button');
 var modalOverlay = document.querySelector('.focus-modal-overlay');
 var cancelButton = document.querySelector('.focus-modal-cancel');
 
@@ -10,9 +10,40 @@ cancelButton.addEventListener('click', close);
 // https://github.com/jkup/focusable
 
 function open() {
+  var previouslyFocused = document.activeElement;
+  var focusableElements = modal.querySelectorAll('a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable], audio[controls], video[controls], summary');
+  focusableElements = Array.prototype.slice.call(focusableElements);
+
+  var firstItem = focusableElements[0];
+  var lastItem = focusableElements[focusableElements.length - 1];
+
+  modal.addEventListener('keydown', trap);
+
   // Show the modal and overlay
   modal.style.display = 'block';
   modalOverlay.style.display = 'block';
+
+  function trap(event) {
+    if (event.keyCode === 9) {
+      //  shift is held down
+      if (event.shiftKey) {
+        // backwards
+        if (document.activeElement === firstItem) {
+          event.preventDefault();
+          lastItem.focus();
+        }
+      } else {
+        //  shift is not held down
+        if (document.activeElement === lastItem) {
+          event.preventDefault();
+          firstItem.focus();
+        }
+      }
+    } else if (event.keyCode === 27) {
+       close();
+    }
+
+  }
 }
 
 function close() {
